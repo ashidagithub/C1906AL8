a29d50e50e168b38453455c154cc117a12cc4b90
Mike. Shi <1019726435@qq.com>
2019-07-15 23:15:49 +0800

    ver1

--- poker3/main.py
+++ poker3/main.py
@@ -14,12 +14,60 @@ import os
 
 # import our modules
 from game.menu import *
-
+from machine.std_mach import *
+from dealer.mike import *
 
 # ----------
+# 打印开始界面
 prt_start()
-time.sleep(3)  # 延迟3秒
+time.sleep(1)  # 延迟3秒
 
-clear_menu()
+# 打印选择游戏玩法界面
 prt_menu_a()
 game_type = int(input())
+
+# 制作所需要的牌
+deck = []
+if (game_type == 1 or game_type == 2 or game_type == 3 or game_type == 4):
+    make_deck_by_type(game_type, deck)
+else:
+    prt_end()
+    exit()
+
+# 预先准备4+1个位置放牌
+player_a = []
+player_b = []
+player_c = []
+player_d = []
+player_dumy = []  # 放置预留牌的位置
+
+# 按游戏类型发牌
+if game_type == 1:
+    deal_to_multi_players(deck, player_a, player_b, player_c)
+    record_deck(player_a, '争上游01副牌.txt')
+    record_deck(player_b, '争上游02副牌.txt')
+    record_deck(player_c, '争上游03副牌.txt')
+if game_type == 2:
+    deal_to_multi_players(deck, player_a, player_b, player_c, player_d)
+    record_deck(player_a, '桥牌01副牌.txt')
+    record_deck(player_b, '桥牌02副牌.txt')
+    record_deck(player_c, '桥牌03副牌.txt')
+    record_deck(player_d, '桥牌04副牌.txt')
+if game_type == 3:
+    deal_to_multi_players_remain(
+        deck, 3, player_dumy, player_a, player_b, player_c)
+    record_deck(player_a, '三人斗地主01副牌.txt')
+    record_deck(player_b, '三人斗地主02副牌.txt')
+    record_deck(player_c, '三人斗地主03副牌.txt')
+    record_deck(player_dumy, '三人斗地主-预留牌.txt')
+if game_type == 4:
+    deal_to_multi_players_remain(
+        deck, 8, player_dumy, player_a, player_b, player_c, player_d)
+    record_deck(player_a, '四人斗地主01副牌.txt')
+    record_deck(player_b, '四人斗地主02副牌.txt')
+    record_deck(player_c, '四人斗地主03副牌.txt')
+    record_deck(player_d, '四人斗地主04副牌.txt')
+    record_deck(player_dumy, '四人斗地主-预留牌.txt')
+
+
+prt_menu_b(game_type)
